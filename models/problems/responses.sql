3602d77d004554999581132873367a01850d7563
Sara Burns <sburns@axim.org>
2025-03-19 15:16:04 -0400

    wip

--- models/problems/responses.sql
+++ models/problems/responses.sql
@@ -17,9 +17,20 @@
 
 with
     responses as (
-        select emission_time, org, course_key, object_id, problem_id, actor_id, success
+        select emission_time, org, course_key, object_id, problem_id, actor_id, success, attempts
         from {{ ref("problem_events") }}
         where verb_id = 'https://w3id.org/xapi/acrossx/verbs/evaluated'
+    ),
+    first_success_time as (
+        select
+            org,
+            course_key,
+            problem_id,
+            actor_id,
+            case when success then emission_time else null end as success_time,
+            attempts,
+            emission_time
+        from responses
     ),
     response_status as (
         select
@@ -27,9 +38,11 @@ with
             course_key,
             problem_id,
             actor_id,
-            MIN(case when success then emission_time else NULL end) as first_success_at,
+            argMin(attempts, success_time) as first_success_attempt,
+            MIN(success_time) as first_success_at,
+            argMax(attempts, emission_time) as last_attempt,
             MAX(emission_time) as last_attempt_at
-        from responses
+        from first_success_time
         group by org, course_key, problem_id, actor_id
     )
 select
@@ -39,5 +52,7 @@ select
     actor_id,
     first_success_at,
     last_attempt_at,
+    first_success_attempt,
+    last_attempt,
     coalesce(first_success_at, last_attempt_at) as emission_time
 from response_status
